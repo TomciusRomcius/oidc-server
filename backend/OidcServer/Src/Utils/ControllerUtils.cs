d3e47b68a4ed4c606491682a03c31f6a8aa52530
Tomas <tomas.ew88@gmail.com>
2026-05-10 12:53:17 +0300

    ResultErrors to controlle API response

---
+++ backend/OidcServer/Src/Utils/ControllerUtils.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OidcServer.Utils;
+
+public static class ControllerUtils
+{
+    public static IActionResult ResultErrorToResponse(ResultError error)
+    {
+        int statusCode = ResultErrorTypeToStatusCode(error.ErrorType);
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = error.Message,
+            Detail = error.Message,
+            Status = statusCode
+        };
+
+        return new ObjectResult(problemDetails)
+        {
+            StatusCode = statusCode
+        };
+    }
+
+    private static int ResultErrorTypeToStatusCode(ResultErrorType errorType)
+    {
+        return errorType switch
+        {
+            ResultErrorType.Validation => 400,
+            ResultErrorType.Forbidden => 403,
+            _ => 500
+        };
+    }
+}
