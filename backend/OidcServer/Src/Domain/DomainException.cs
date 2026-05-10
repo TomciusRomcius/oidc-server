81ee8b1205e4e8c765ce09a9fe4e340840133c50
Tomas <tomas.ew88@gmail.com>
2026-05-10 12:53:31 +0300

    DomainException

---
+++ backend/OidcServer/Src/Domain/DomainException.cs
@@ -0,0 +1,19 @@
+namespace OidcServer.Domain;
+
+[Serializable]
+public class DomainException : Exception
+{
+    public DomainException()
+    {
+    }
+
+    public DomainException(string message)
+        : base(message)
+    {
+    }
+
+    public DomainException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
